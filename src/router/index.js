9c70ce023333fe9f73c8be5b71da31503bf06ed0
Petr Czepiec <petr@czepiec.me>
2025-03-09 21:52:19 +0100

    [WIP]

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createWebHistory, createRouter } from 'vue-router';
 import routes from './routes';
 
 const router = createRouter({
-	history: createWebHistory(),
+        history: createWebHistory(new URL(import.meta.env.BASE_URL).pathname),
 	routes: routes,
 });
 
